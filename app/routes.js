ed7002a58663d1d2b8f176524ad31407a8662eb6
wdurhamh <wdurhamh@gmail.com>
2015-03-26 15:57:54 +0000

    new routes

--- app/routes.js
+++ app/routes.js
@@ -17,6 +17,77 @@ module.exports = function(app) {
 		    });
 	});
 
+
+app.post('/login', function(req,res){
+        var jsonData = "";
+        req.on('data',function (chunk){
+                jsonData += chunk;
+        });
+        req.on('end',function(){
+                var lgnObj = JSON.parse(jsonData);
+                var username = lgnObj.username;
+                var pswrd = lgnObj.password;
+
+                        users.findOne({username:username},function(err,user){
+                                if(err) throw err;
+				console.log(user);
+                                user.toArray(function(err,usrArray){
+                                        console.log(usrArray);
+                                        if(usrArray.length==0){
+                                                console.log('username not found');
+                                                res.writeHead(200);
+                                                res.json([]);
+                                        }
+                                        else if(usrArray[0].password==pswrd){
+                                                console.log('successful login');
+                                                res.writeHead(200);
+                                                res.end('success');
+                                        }
+                                        else{
+                                                console.log('incorrect password');
+                                                res.writeHead(200);
+                                                res.end('incorrect password');
+                                        }
+                                });
+                        });
+                });
+
+        });
+
+app.post('/register',function(req,res){
+        var jsonData = "";
+        req.on('data',function (chunk){
+                jsonData += chunk;
+        });
+        req.on('end',function(){
+                var regObj = JSON.parse(jsonData);
+                var name_f = regObj.first;
+                var name_l = regObj.last;
+                var username = regObj.username;
+                var pswrd = regObj.password;
+		console.log(regObj);
+                users.findOne({username:username},function(err,result){
+                	if(err) throw err;
+                	result.toArray(function(err,itemArr){
+                        	if(itemArr.length>0){
+                        		console.log('username in use');
+                                        console.log(itemArr);
+                                        res.writeHead(200);
+                                        res.end('invalid');
+                                }
+                                else{
+                                        users.insert(regObj,function(err,records){
+                                        	console.log('Added user '+ username);
+                                        });
+                                }
+                        });
+                });
+        });
+});
+
+
+
+
     // application ------------------------------------
     app.get('*', function(req, res) {
 	    res.sendfile('./html/index.html');
