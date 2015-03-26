9831acf6c6e2598546ee07d8c62070b81624f077
Danny North <dnorth2@hotmail.com>
2015-03-26 17:05:50 +0000

    fixed moving stuff, it should all work now

--- provopantry_server.js
+++ provopantry_server.js
@@ -10,7 +10,7 @@ var db = require('./config/database');
 mongoose.connect(db.url);
 
 app.use('/', express.static('./app'));
-
+app.use('/assets', express.static('./assets'));
 // routes ====================================================            
 require('./app/routes.js')(app);
 
