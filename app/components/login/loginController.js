6e983dd7ad036fa1e4b31a6fcac194b58a7f5b9a
Danny North <dnorth2@hotmail.com>
2015-04-01 16:51:33 +0000

    small change to login view and register size

--- app/components/login/loginController.js
+++ app/components/login/loginController.js
@@ -1,6 +1,17 @@
 angular.module('provoPantry').controller('LoginController', 
     ['$scope', '$http', function($scope, $http){
         $scope.test = "PROVO PANTRY FOR LIFE!";
+
+	$scope.validateUser = function(){
+	    
+	    //TODO validate properly
+	    if($scope.username == "dnorth2" && $scope.password == "test"){
+		alert("Validated!!");
+	    }
+	    else {
+		alert("Not Validated! try username: dnorth2 and password: test");
+	    }
+	}
         
         $scope.submitRegistration = function(){
             alert("Registered!!");
