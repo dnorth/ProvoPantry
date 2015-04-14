9a85e3937f82e3cbe878bd35faa79175b89d0913
Danny North <dnorth2@hotmail.com>
2015-04-14 17:33:06 +0000

    logout returns

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,7 @@ var User = mongoose.model('users');
 var usrCntrl = require('./controllers/user_controller');
 
 module.exports = function(router) {
-
+    console.log("got into router");
     // api -------------------------------------------
     router.get('/api/v1/users/:username', function(req, res) {
 	    var userName = req.param('username');
@@ -23,60 +23,59 @@ module.exports = function(router) {
 	});
 
 	router.post('/api/v1/favorite',function(req, res){
-               console.log("in favorites route");
+		console.log("in favorites route");
 		
-                    var regObj = req.body;	
-                    //console.log(regObj);             
-		    var user_id = req.session.user;
-		    var id = regObj.id;
-		    var user_name = req.session.username;
-		    console.log(id);	
-		     users.findOne({username:req.session.username},function(err,result){
+		var regObj = req.body;	
+		//console.log(regObj);             
+		var user_id = req.session.user;
+		var id = regObj.id;
+		var user_name = req.session.username;
+		console.log(id);	
+		users.findOne({username:req.session.username},function(err,result){
 			if(err) throw err;
 		        if(result != null)
-			{
-			     var user = new users(result);
-			     console.log(user) 
-                             console.log('found the user');
-			     //check if user has already faved it
-			     var favorites_array = user.favorites;
-			     
-			     var contains = false;
-			     for(var i = 0; i < favorites_array.length; i++)
-				{
-				   if(favorites_array[i].id = id)
-					{
-						contains = true;
-					}
+			    {
+				var user = new users(result);
+				console.log(user); 
+				console.log('found the user');
+				//check if user has already faved it
+				var favorites_array = user.favorites;
 				
-				}
-			     if(!contains)
-			      {
-				user.favorites.push(regObj);
-				user.update({favorites:user.favorites},function(err){
-                                          
-				    if(err)
-					console.log(err);
-				    else
-					console.log("added to faovrites");
-				});
-				console.log(user.favorites.length);	
-			      }
+				var contains = false;
+				for(var i = 0; i < favorites_array.length; i++)
+				    {
+					if(favorites_array[i].id = id)
+					    {
+						contains = true;
+					    }
+					
+				    }
+				if(!contains)
+				    {
+					user.favorites.push(regObj);
+					user.update({favorites:user.favorites},function(err){
+						
+						if(err)
+						    console.log(err);
+						else
+						    console.log("added to faovrites");
+					    });
+					console.log(user.favorites.length);	
+				    }
 				else
-			      {
-				console.log("item has already been favorited");	
-			      }
-			    
-			     res.writeHead(200);     		
-			}	           
+				    {
+					console.log("item has already been favorited");	
+				    }
+				res.end();     		
+			    }	           
 			else
-			{
-			  console.log('user does not exist');
-			   res.writeHead(200);
-			   res.end('invalid');	
-			}
+			    {
+				console.log('user does not exist');
+				res.writeHead(200);
+				res.end('invalid');	
+			    }
+		    });
 	    });
-	});
 
 	router.post('/api/v1/login',usrCntrl.login);
 	router.post('/api/v1/register',usrCntrl.register);
